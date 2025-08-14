5fe0d07132b9d51e8da076958662cda06fecca35
Thiago Augusto <wstagf@gmail.com>
2025-08-14 20:22:34 -0300

    Executado exercicios 1, 2 e 3

---
+++ Aula001/ex02.py
@@ -0,0 +1,15 @@
+# Faça um Programa que peça um número e então mostre a mensagem:
+# O número informado foi [número]. 
+
+numeroArmazenadoComoString = input("Digite um valor: ")
+
+print("O número informado foi " + 
+      numeroArmazenadoComoString )
+
+
+# CONCATENACAO = juntar duas ou mais palavras
+# endreco completo - rua, numero, quadra, lot
+# nome completo - nome sobrenome
+
+
+
---
+++ Aula001/ex03.py
@@ -0,0 +1,12 @@
+# Faça um Programa que peça 
+# dois números e imprima a soma.
+
+numero1Texto = input("Digite um valor: ")
+numero2Texto = input("Digite um valor: ")
+
+soma = float(numero1Texto) + float(numero2Texto)
+
+print("A soma dos dois valores deu: " + str(soma))
+
+
+print(f"A soma dos dois valores deu: {soma}" )
