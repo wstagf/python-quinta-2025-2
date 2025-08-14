1c89151a2066eccc4ec9a46fe723b49bc9d4859e
Thiago Augusto <wstagf@gmail.com>
2025-08-14 20:50:39 -0300

    exercicios 4

---
+++ Aula001/ex04.py
@@ -0,0 +1,16 @@
+# Faça um Programa que peça
+# as 4 notas bimestrais e mostre a média.
+
+
+n1 = input("Digite a primeira nota: ") 
+n2 = input("Digite a segunda nota: ")
+n3 = input("Digite a terceira nota: ")
+n4 = input("Digite a quarta nota: ")
+
+
+media = (float(n1) +  float(n2) +  float(n3)  +  float(n4)) / 4
+
+print("A media é: " + str(media))
+
+
+
