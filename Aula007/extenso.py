69e07e5e4889c800f12e939beaba27ef75d4d5bc
Thiago Augusto <wstagf@gmail.com>
2025-11-06 20:40:18 -0300

    Aula de Bibliotecas python

---
+++ Aula007/extenso.py
@@ -0,0 +1,16 @@
+import numero_por_extenso
+
+def main():
+    #print("Hello, World!")
+    numero = float(input("Digite um valor: "))
+
+    print(numero_por_extenso.real(numero))
+
+   
+
+    #print(f"O número é: {numero}")
+    #print("Trezentos e cinquenta e seis")
+    
+
+if __name__ == "__main__":
+    main()
